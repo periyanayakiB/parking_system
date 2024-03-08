bf5207ec6425781844c9a05319991a0c2450612a
periyanayakiB <periyanayaki@steam-a.com>
2024-03-08 13:05:49 +0530

    initial

--- Backend/apiindex.js
+++ Backend/apiindex.js
@@ -1,21 +1,8 @@
-// //apiindex.js
-// import express from 'express';
-// import { router } from "./routes.js";
-// const app = express();
-// app.use(express.json());
-// app.use(router);
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
 import express from 'express';
 import session from 'express-session';
 import { router } from './routes.js';
 
 const app = express();
-
 
 app.use(session({
     secret: 'your-secret', 
